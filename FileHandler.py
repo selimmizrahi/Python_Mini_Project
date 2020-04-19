95bffd5e6ec8c946f64e298e06022cc8b3c5a7f1
selimmizrahi <mizrahiselim@gmail.com>
2020-04-19 17:06:55 +0300

    "Exercise 2"

---
+++ FileHandler.py
@@ -0,0 +1,29 @@
+import csv
+
+
+class Filehandler:
+    def __init__(self):
+        self.users = []
+
+    def load_from_cvs(self, *file_name):
+        try:
+            with open(file_name[0]) as csv_file:
+                csv_reader = csv.reader(csv_file)
+                for row in csv_reader:
+                        users = {
+                            "id": row[0],
+                            "first name": row[1],
+                            "last name": row[2],
+                            "password": row[3],
+                            "position": row[4],
+                            "salary": row[5],
+                            "role": row[6],
+                        }
+                        self.users.append(users)
+
+        except Exception as error:
+            print("Hi Error, Welcome!" + str(error))
+
+file = Filehandler()
+file.load_from_cvs("/Users/selimmizrahi/Desktop/Python_Mini_Project/cvs_file/user.csv")
+print(file.users)
