3037608d799b22e16257f32ba29cde57267d7444
selimmizrahi <mizrahiselim@gmail.com>
2020-04-20 19:22:10 +0300

    Update on 3rd milestone

--- User.py
+++ User.py
@@ -1,28 +1,30 @@
-import csv
+from FileHandler import File_handler
+
 
 class User:
+    def __init__(self):
+        self.handler = File_handler()
 
-    def inputResult(self):
-        self.User = User
-        self.username = str(input('Please insert your username: '))
-        self.password = str(input('Please insert your password: '))
+    def user_auth(self, username, password):
+        self.handler.load_from_csv('/Users/selimmizrahi/Desktop/Python_Mini_Project/user.csv')
+        print(self.handler.list)
 
-    def user_auth(username, password):
-        with open("/Users/selimmizrahi/Desktop/Python_Mini_Project/cvs_file/user.csv") as csv_file:
-            csv_reader = csv.reader(csv_file)
-            access = False
-            user_role = ' '
-            for row in csv_reader:
-                if username.lower() == row[1] and password.lower() == row[3]:
-                    access = True
-                    user_role = str(row[6])
+        access = False
+        user_role = ' '
+
+        for row in self.handler.list:
+            if username == row["first_name"] and password == row["password"]:
+                access = True
+                user_role = str(row["role"])
+
             if access:
                 print('Access Granted')
                 print(f'Your role is {user_role}')
                 return access
+
             else:
                 print('Access Denied')
                 return access
 
-User.inputResult(User)
-User.user_auth(User.username, User.password)
+User.__init__(User)
+User.user_auth(User, "Amir", '12345678')
