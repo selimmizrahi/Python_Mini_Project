e279cd546f8ba85d29ee27b813f73c089b300079
selimmizrahi <mizrahiselim@gmail.com>
2020-04-20 18:39:01 +0300

    4th milestone

--- FileHandler.py
+++ FileHandler.py
@@ -1,7 +1,11 @@
 import csv
 from csv import writer
 
-class Filehandler:
+
+class File_handler:
+
+    def __init__(self):
+        self.list = []
 
     def load_from_cvs(self, file_name):
         try:
@@ -9,25 +13,30 @@ class Filehandler:
                 csv_reader = csv.DictReader(csv_file)
 
                 for line in csv_reader:
-                    print(line)
+                    self.list.append(line)
 
         except Exception as error:
-            print("Hi Error, Welcome!" + str(error))
+            print("There is an error :" + str(error))
 
     def append_to_csv(self, file_name, data):
         try:
+
+            self.load_from_cvs(file_name)
+            for row in self.list:
+                if row.get("id") == data[0]:
+                    raise Exception("This ID already exists")
+
             with open(file_name, 'a+', newline='') as write_obj:
                 csv_writer = writer(write_obj)
                 csv_writer.writerow(data)
 
-            for row in csv_writer:
-                print(row[0])
-
         except Exception as error:
-            print("There is" + str(error))
+            print("There is an error :" + str(error))
 
-new_data = [8, "Selim", "Mizo", "password", "student", 100, "student"]
 
-file = Filehandler()
-file.append_to_csv("/Users/selimmizrahi/Desktop/Python_Mini_Project/cvs_file/user.csv",new_data)
-file.load_from_cvs("/Users/selimmizrahi/Desktop/Python_Mini_Project/cvs_file/user.csv")
+new_data = ["1", "Selim", "Mizo", "password", "student", 100, "student"]
+
+file = File_handler()
+file.append_to_csv('/Users/selimmizrahi/Desktop/Python_Mini_Project/user.csv',new_data)
+file.load_from_cvs('/Users/selimmizrahi/Desktop/Python_Mini_Project/user.csv')
+
