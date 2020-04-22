a2f40d210477efa3a0164e662628638b490c7a16
selimmizrahi <mizrahiselim@gmail.com>
2020-04-22 21:29:09 +0300

    Day 2 Exercise 2

--- User.py
+++ User.py
@@ -2,29 +2,29 @@ from FileHandler import File_handler
 
 
 class User:
-    def __init__(self):
-        self.handler = File_handler()
+    def __init__(self,file_name):
+        self.handler = File_handler(file_name)
 
     def user_auth(self, username, password):
         self.handler.load_from_csv('/Users/selimmizrahi/Desktop/Python_Mini_Project/user.csv')
-        print(self.handler.list)
-
+        res = {}
         access = False
         user_role = ' '
 
-        for row in self.handler.list:
+        for row in self.handler.data:
+            res.update(row)
             if username == row["first_name"] and password == row["password"]:
                 access = True
                 user_role = str(row["role"])
 
-            if access:
-                print('Access Granted')
-                print(f'Your role is {user_role}')
-                return access
+                if access:
+                    print('Access Granted')
+                    print(f'Your role is {user_role}')
+                    return user_role
 
-            else:
-                print('Access Denied')
-                return access
+                else:
+                    print('Access Denied')
+                    return access
 
-User.__init__(User)
-User.user_auth(User, "Amir", '12345678')
+User.__init__(User,'/Users/selimmizrahi/Desktop/Python_Mini_Project/user.csv')
+# User.user_auth(User, "Hen", 'password')
